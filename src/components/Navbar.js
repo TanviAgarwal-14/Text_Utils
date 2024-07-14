9dc19a84e014300e378a2de9be5519e8ec1bb4ac
TanviAgarwal-14 <tanvi.23bcs10143@ms.sst.scaler.com>
2024-07-14 18:42:42 +0530

    my commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
     <a className="navbar-brand" href="/">{props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -11,17 +11,17 @@ export default function Navbar(props) {
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
+        {/* <li className="nav-item">
           <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
+        </li> */}
+        {/* <li className="nav-item">
           <a className="nav-link" href="/">{props.about}</a>
-        </li>
+        </li> */}
       </ul>
-      <form className="d-flex" role="search">
+      {/* <form className="d-flex" role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
         <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+      </form> */}
     </div>
   </div>
 </nav>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,5 +1,4 @@
 // import PropTypes from 'prop-types';
-import { set } from 'mongoose';
 import React, {useState} from 'react'
 
 export default function TextForm(props) {
@@ -7,22 +6,70 @@ export default function TextForm(props) {
         let newText= text.toUpperCase();
         setText(newText);
     }
+
+    const handleLoClick=()=>{
+      let newText= text.toLowerCase();
+      setText(newText);
+    }
+
+    const handleClearClick=()=>{
+      let newText= '';
+      setText(newText);
+    }
+    const handleCopyText = () => {
+      navigator.clipboard.writeText(text)
+          .then(() => {
+              alert('Text copied to clipboard!');
+          }) 
+    };
+    const handleSentenceCase = () => {
+      if (text.trim().length === 0) {
+          return;
+      }
+  
+      const words = text.split(' ').map(word => {
+          if (word.length > 0) {
+              return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+          }
+          return '';
+      });
+  
+      const newText = words.join(' ');
+      setText(newText);
+    };
+    const handleExtraSpace=()=>{
+      let newText=text.split(/[ ]+/);
+      setText(newText.join(" "))
+    } 
     const handleOnChange=(event) =>{
         setText(event.target.value);
     }
 
-    const [text, setText] =useState('Enter the text');
+    const [text, setText] =useState('');
     //agar value change kerna hai text ki 
     //text = "new text" //very wrong way to change the state
     //setText= "new text" //correct way to change the state
   return (
-<div>
-    <h1>{props.heading}</h1>
-    <div className="mb-3">
-  {/* <label for="mybox" class="form-label">Enter your Text Below!</label> */}
-    <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="7"></textarea>
+    <>
+    <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="7"></textarea>
+        </div>
+      <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+      <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
+      <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
+      <button className="btn btn-primary mx-2" onClick={handleCopyText}>Copy to ClipBoard</button>
+      <button className="btn btn-primary mx-2" onClick={handleSentenceCase}>Convert to Senetnce Case</button>
+      <button className="btn btn-primary mx-2" onClick={handleExtraSpace}>Remove extra Spaces</button>
     </div>
-    <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
-</div>
+    <div className="container my-4">
+      <h2>Your Text Summary</h2>
+      <p>{text.split(" ").length} and {text.length} characters</p>
+      <p>read in {0.008 * text.split(" ").length } minutes</p>
+      <h3>Preview</h3>
+      <p>{text}</p>
+    </div>
+    </>
   )
 }
